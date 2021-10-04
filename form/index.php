8aef8f7be3f295f125e2bccb34aea254b529744b
vovan-adam <86118642+vovan-adam@users.noreply.github.com>
2021-10-04 20:43:19 +0200

    Delete form directory

--- form/index.php
+++
@@ -1,9 +0,0 @@
-<form action="" method="">
-	
-	<input type="text" name="login" placeholder="Введите логин" required>
-	<input type="password" name="pass" placeholder="Введите пароль" required>
-	<input type="email" name="email" placeholder="Введите e-mail" required>
-	<input type="tel" name="tel" placeholder="Введите телефон">
-
-	<input type="submit" value="Отправить">
-</form>
